be25ad776f47534b01a1f21fb240a8bbb8131668
Dmitri Orlov <dmitri.orlov.anatolevich@gmail.com>
2024-09-12 14:40:21 +0600

    change

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(cors({
-    origin: 'https://orlov-dmitri.github.io/new-cost/',
-}));
+app.use(cors());
 
 app.use("/api", require("./routes/cost"));
 
